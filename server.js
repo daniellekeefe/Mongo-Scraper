dd5cd95e8a21318bda4b11c13cf45ce56a75c906
Carmen Owen <doctorsout@yahoo.com>
2018-09-12 16:08:26 -0400

    deployment attempt

--- server.js
+++ server.js
@@ -7,11 +7,9 @@ const bodyParser = require('body-parser');
 
 
 var PORT = process.env.PORT || 3030;
-var MONGODB_URI = process.env.MONGODB_URI;
-
-mongoose.connect("mongodb://localhost/LandScraper", { useNewUrlParser: true });
 
 const app = express();
+
 
 app.use(express.static("public"));
 
@@ -22,6 +20,13 @@ app.use(bodyParser.text({ type: 'text/html' }));
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
+
+
+//var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/LandScraper";
+
+//mongoose.connect(MONGODB_URI);
+
+mongoose.connect(MONGODB_URI || "mongodb://localhost/LandScraper", { useNewUrlParser: true });
 
 app.listen(PORT, function() {
     console.log("App running on port " + PORT);
